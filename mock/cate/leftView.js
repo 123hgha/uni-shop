238c874e5edfdc1d986c3822e37aa26a2097f570
hgh <2754976216@qq.com>
2022-05-13 16:49:14 +0800

    完成了分类页面的开发

---
+++ mock/cate/leftView.js
@@ -0,0 +1,289 @@
+const leftView = [{
+  "message": [{
+      "cat_id": 1,
+      "cat_name": "大家电",
+      "cat_pid": 0,
+      "cat_level": 0,
+      "cat_deleted": false,
+      "cat_icon": "",
+      "children": [{
+          "cat_id": 3,
+          "cat_name": "电视",
+          "cat_pid": 1,
+          "cat_level": 1,
+          "cat_deleted": false,
+          "cat_icon": "",
+          "children": [{
+            "cat_id": 1,
+            "cat_name": "曲面电视",
+            "cat_pid": 3,
+            "cat_level": 2,
+            "cat_deleted": false,
+            "cat_icon": "../../static/images/floor/floor1/product_title1.jpg"
+          }]
+        },
+        {
+          "cat_id": 3,
+          "cat_name": "空调",
+          "cat_pid": 1,
+          "cat_level": 1,
+          "cat_deleted": false,
+          "cat_icon": "",
+          "children": [{
+            "cat_id": 2,
+            "cat_name": "大空调",
+            "cat_pid": 3,
+            "cat_level": 2,
+            "cat_deleted": false,
+            "cat_icon": "../../static/images/floor/floor1/product_title1.jpg"
+          }]
+        },
+        
+        {
+          "cat_id": 3,
+          "cat_name": "洗衣机",
+          "cat_pid": 1,
+          "cat_level": 1,
+          "cat_deleted": false,
+          "cat_icon": "",
+          "children": [{
+            "cat_id": 3,
+            "cat_name": "大洗衣机",
+            "cat_pid": 3,
+            "cat_level": 2,
+            "cat_deleted": false,
+            "cat_icon": "../../static/images/floor/floor1/product_title1.jpg"
+          }]
+        },
+        
+        {
+          "cat_id": 3,
+          "cat_name": "冰箱",
+          "cat_pid": 1,
+          "cat_level": 1,
+          "cat_deleted": false,
+          "cat_icon": "",
+          "children": [{
+            "cat_id": 4,
+            "cat_name": "大冰箱",
+            "cat_pid": 3,
+            "cat_level": 2,
+            "cat_deleted": false,
+            "cat_icon": "../../static/images/floor/floor1/product_title1.jpg"
+          }]
+        },
+        {
+          "cat_id": 3,
+          "cat_name": "冰箱",
+          "cat_pid": 1,
+          "cat_level": 1,
+          "cat_deleted": false,
+          "cat_icon": "",
+          "children": [{
+            "cat_id": 5,
+            "cat_name": "大冰箱",
+            "cat_pid": 3,
+            "cat_level": 2,
+            "cat_deleted": false,
+            "cat_icon": "../../static/images/floor/floor1/product_title1.jpg"
+          }]
+        },
+        {
+          "cat_id": 3,
+          "cat_name": "冰箱",
+          "cat_pid": 1,
+          "cat_level": 1,
+          "cat_deleted": false,
+          "cat_icon": "",
+          "children": [{
+            "cat_id": 5,
+            "cat_name": "大冰箱",
+            "cat_pid": 3,
+            "cat_level": 2,
+            "cat_deleted": false,
+            "cat_icon": "../../static/images/floor/floor1/product_title1.jpg"
+          }]
+        },
+      ]
+    },
+
+    {
+      "cat_id": 2,
+      "cat_name": "热门推荐",
+      "cat_pid": 0,
+      "cat_level": 0,
+      "cat_deleted": false,
+      "cat_icon": "",
+      "children": [{
+        "cat_id": 3,
+        "cat_name": "你想要的",
+        "cat_pid": 1,
+        "cat_level": 1,
+        "cat_deleted": false,
+        "cat_icon": "",
+        "children": [{
+          "cat_id": 5,
+          "cat_name": "曲面电视",
+          "cat_pid": 3,
+          "cat_level": 2,
+          "cat_deleted": false,
+          "cat_icon": "../../static/images/floor/floor1/product_title1.jpg"
+        }]
+      }]
+    },
+
+    {
+      "cat_id": 3,
+      "cat_name": "海外购",
+      "cat_pid": 0,
+      "cat_level": 0,
+      "cat_deleted": false,
+      "cat_icon": "",
+      "children": [{
+        "cat_id": 3,
+        "cat_name": "海外特产",
+        "cat_pid": 1,
+        "cat_level": 1,
+        "cat_deleted": false,
+        "cat_icon": "",
+        "children": [{
+          "cat_id": 5,
+          "cat_name": "曲面电视",
+          "cat_pid": 3,
+          "cat_level": 2,
+          "cat_deleted": false,
+          "cat_icon": "../../static/images/floor/floor1/product_title1.jpg"
+        }]
+      }]
+    },
+
+    {
+      "cat_id": 4,
+      "cat_name": "苏宁房产",
+      "cat_pid": 0,
+      "cat_level": 0,
+      "cat_deleted": false,
+      "cat_icon": "",
+      "children": [{
+        "cat_id": 3,
+        "cat_name": "高楼大厦",
+        "cat_pid": 1,
+        "cat_level": 1,
+        "cat_deleted": false,
+        "cat_icon": "",
+        "children": [{
+          "cat_id": 5,
+          "cat_name": "曲面电视",
+          "cat_pid": 3,
+          "cat_level": 2,
+          "cat_deleted": false,
+          "cat_icon": "../../static/images/floor/floor1/product_title1.jpg"
+        }]
+      }]
+    },
+
+    {
+      "cat_id": 5,
+      "cat_name": "居家生活",
+      "cat_pid": 0,
+      "cat_level": 0,
+      "cat_deleted": false,
+      "cat_icon": "",
+      "children": [{
+        "cat_id": 3,
+        "cat_name": "生活用品",
+        "cat_pid": 1,
+        "cat_level": 1,
+        "cat_deleted": false,
+        "cat_icon": "",
+        "children": [{
+          "cat_id": 5,
+          "cat_name": "曲面电视",
+          "cat_pid": 3,
+          "cat_level": 2,
+          "cat_deleted": false,
+          "cat_icon": "../../static/images/floor/floor1/product_title1.jpg"
+        }]
+      }]
+    },
+
+    {
+      "cat_id": 6,
+      "cat_name": "食品酒水",
+      "cat_pid": 0,
+      "cat_level": 0,
+      "cat_deleted": false,
+      "cat_icon": "",
+      "children": [{
+        "cat_id": 3,
+        "cat_name": "吃的喝的",
+        "cat_pid": 1,
+        "cat_level": 1,
+        "cat_deleted": false,
+        "cat_icon": "",
+        "children": [{
+          "cat_id": 5,
+          "cat_name": "曲面电视",
+          "cat_pid": 3,
+          "cat_level": 2,
+          "cat_deleted": false,
+          "cat_icon": "../../static/images/floor/floor1/product_title1.jpg"
+        }]
+      }]
+    },
+
+    {
+      "cat_id": 7,
+      "cat_name": "电脑办公",
+      "cat_pid": 0,
+      "cat_level": 0,
+      "cat_deleted": false,
+      "cat_icon": "",
+      "children": [{
+        "cat_id": 3,
+        "cat_name": "平板电脑",
+        "cat_pid": 1,
+        "cat_level": 1,
+        "cat_deleted": false,
+        "cat_icon": "",
+        "children": [{
+          "cat_id": 5,
+          "cat_name": "曲面电视",
+          "cat_pid": 3,
+          "cat_level": 2,
+          "cat_deleted": false,
+          "cat_icon": "../../static/images/floor/floor1/product_title1.jpg"
+        }]
+      }]
+    },
+
+    {
+      "cat_id": 8,
+      "cat_name": "手机相机",
+      "cat_pid": 0,
+      "cat_level": 0,
+      "cat_deleted": false,
+      "cat_icon": "",
+      "children": [{
+        "cat_id": 3,
+        "cat_name": "手机相机",
+        "cat_pid": 1,
+        "cat_level": 1,
+        "cat_deleted": false,
+        "cat_icon": "",
+        "children": [{
+          "cat_id": 5,
+          "cat_name": "曲面电视",
+          "cat_pid": 3,
+          "cat_level": 2,
+          "cat_deleted": false,
+          "cat_icon": "../../static/images/floor/floor1/product_title1.jpg"
+        }]
+      }]
+    },
+  ]
+}]
+
+export {
+  leftView
+}

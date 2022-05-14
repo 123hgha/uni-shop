32af940ed2aef0768ec133ca804c078f1c9e1b2d
hgh <2754976216@qq.com>
2022-05-14 17:59:49 +0800

    完成了商品详情页面的开发

---
+++ mock/detail/detail.js
@@ -0,0 +1,56 @@
+const detail = [{
+  "message": {
+    "goods_id": 57332,
+    "cat_id": 998,
+    "goods_name": "400毫升 海鲜食品冷藏冰包 注水冰袋医用冰袋户外冷藏保鲜熟食冷藏反复使用（10个装）",
+    "goods_price": 14,
+    "goods_number": 100,
+    "goods_weight": 100,
+    "goods_introduce": "<div class=\"lazyimg\"><!-- Save for Web Slices (20170730关联模板.psd) -->\n<p><img data-src=\"//image.suning.cn/uimg/sop/commodity/167890268714163515505319_x.jpg?from=mobile&amp;format=80q.webp\" alt=\"\" usemap=\"#Map\" border=\"0\" src=\"//image.suning.cn/uimg/sop/commodity/167890268714163515505319_x.jpg?from=mobile&format=80q.webp\" width=\"100%\" height=\"auto\"> </p>\n<!-- End Save for Web Slices --><p></p><p><img picsize=\"706KB\" data-src=\"//image.suning.cn/uimg/sop/phonecomm/157271695856956094412300_640x.jpg?from=mobile&amp;format=80q.webp\" alt=\"\" src=\"//image.suning.cn/uimg/sop/phonecomm/157271695856956094412300_640x.jpg?from=mobile&format=80q.webp\" width=\"100%\" height=\"auto\"></p><p><img picsize=\"610KB\" data-src=\"//image.suning.cn/uimg/sop/phonecomm/192892939814201549766135_640x.jpg?from=mobile&amp;format=80q.webp\" alt=\"\" src=\"//image.suning.cn/uimg/sop/phonecomm/192892939814201549766135_640x.jpg?from=mobile&format=80q.webp\" width=\"100%\" height=\"auto\"></p></div>",
+    "goods_big_logo": "../../static/images/banner/banner1.jpg",
+    "goods_small_logo": "../../static/images/banner/banner2.jpg",
+    "goods_state": 2,
+    "is_del": "0",
+    "add_time": 1516662792,
+    "upd_time": 1516662792,
+    "delete_time": null,
+    "hot_mumber": 0,
+    "is_promote": false,
+    "cat_one_id": 962,
+    "cat_two_id": 981,
+    "cat_three_id": 998,
+    "goods_cat": "962,981,998",
+    "pics": [{
+      "pics_id": 234751,
+      "goods_id": 57332,
+      "pics_big": ["../../static/images/banner/banner1.jpg", 
+      "../../static/images/banner/banner2.jpg",
+       "../../static/images/banner/banner3.jpg",
+       "../../static/images/banner/banner4.jpg"
+      ],
+      "pics_mid": "../../static/images/banner/banner2.jpg",
+      "pics_sma": "../../static/images/banner/banner3.jpg",
+      "pics_big_url": "../../static/images/banner/banner4.jpg",
+      "pics_mid_url": "../../static/images/banner/banner1.jpg",
+      "pics_sma_url": "../../static/images/banner/banner2.jpg"
+    }],
+    "attrs": [{
+      "goods_id": 57332,
+      "attr_id": 8519,
+      "attr_value": "400",
+      "add_price": 0,
+      "attr_name": "主体-型号",
+      "attr_sel": "only",
+      "attr_write": "manual",
+      "attr_vals": "Q6"
+    }]
+  },
+  "meta": {
+    "msg": "获取成功",
+    "status": 200
+  }
+}]
+
+export {
+  detail
+}
